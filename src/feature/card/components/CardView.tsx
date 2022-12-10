d430b2d6522397b5c0988e006dfb707b0c01669b
Marek <marek@mk-home.pl>
2022-12-10 11:10:45 +0100

    add action buttons to card view

--- src/feature/card/components/CardView.tsx
+++ src/feature/card/components/CardView.tsx
@@ -3,7 +3,10 @@ import {getCardByID} from "../selectors";
 import {ApplicationState} from "../../../app/store";
 import {useNavigate, useParams} from "react-router-dom";
 import ModalLink from "../../../components/helpers/ModalLink";
-import {remove} from "../cardSlice";
+import {Dropdown, DropdownButton} from "react-bootstrap";
+import {getLists} from "../../list/selectors";
+import {List} from "../../list";
+import {moveCard, remove} from "../cardSlice";
 
 
 export default function CardView() {
@@ -11,8 +14,18 @@ export default function CardView() {
     const cardID = Number(params.cardID);
 
     const card = useSelector((state: ApplicationState) => getCardByID(state, Number(cardID)));
+    const lists = useSelector((state: ApplicationState) => getLists(state));
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const moveDropdownElements = lists.filter(list => list.id !== card.listID).map((list: List) => {
+        return <Dropdown.Item style={{fontSize: 14}} onClick={() => doMoveCard(list.id)}>{list.title}</Dropdown.Item>
+    });
+
+    function doMoveCard(listID: number) {
+        dispatch(moveCard(cardID, listID));
+        navigate('/');
+    }
 
     function onDeleteCardButtonHandler() {
         dispatch(remove(cardID));
@@ -25,8 +38,12 @@ export default function CardView() {
                 <ModalLink to={`/cards/${cardID}/update`}>
                     <button className="btn btn-sm btn-primary me-2">Edytuj</button>
                 </ModalLink>
+
                 <button onClick={onDeleteCardButtonHandler} className="btn btn-sm btn-danger me-2">Usuń</button>
-                <button className="btn btn-sm btn-light">Przenieś</button>
+
+                <DropdownButton className="d-inline" size="sm" title="Przenieś" variant="secondary" drop="down">
+                    {moveDropdownElements}
+                </DropdownButton>
             </div>
             <div className="mt-3">
                 <label className="fw-bolder">Tytuł:</label>
